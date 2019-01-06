2b0101ee1e659244cb40fc9b1a57b1a974bcf4dc
Crouston <yosuam34@gmail.com>
2019-01-06 20:05:47 +0700

    Level added

---
+++ main menu kelar/Assets/Script/LevelChooser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChooser : MonoBehaviour {
+
+    public int sceneIndex = 0;
+
+	public void OnCLick()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
